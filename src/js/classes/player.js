5dfa45e80b0b11d64bad5f41c8d89a4671ef7c62
Felipe Barros <48369466+FelipeJBarros@users.noreply.github.com>
2023-03-28 21:03:39 -0300

    Adição do objeto de configurações

--- src/js/classes/player.js
+++ src/js/classes/player.js
@@ -1,59 +1,52 @@
-import { Entity } from './entity.js';
+import { Entity } from "./entity.js";
 
 class Player extends Entity {
-    #direction;
-    constructor(width, height, color) {
-        super(width, height, color);
-        this.#direction = 0;
+  constructor(width, height, color) {
+    super(width, height, color);
+  }
+
+  #setMouseListener(canvas) {
+    canvas.addEventListener("mousemove", (event) => {
+      let rect = canvas.getBoundingClientRect();
+      let root = document.documentElement;
+
+      this.update(event.clientX - rect.left - root.scrollLeft);
+    });
+  }
+
+  getDistanceFromCenter(position) {
+    return position - (this.getPositionX() + this.getWidth() / 2);
+  }
+
+  init(posX, posY, spdX, spdY, canvas) {
+    this.#setMouseListener(canvas);
+    this.setPositionX(posX);
+    this.setPositionY(posY);
+    this.setSpeedX(spdX);
+    this.setSpeedY(spdY);
+  }
+
+  render(context) {
+    context.fillStyle = this.getColor();
+    context.fillRect(
+      this.getPositionX(),
+      this.getPositionY(),
+      this.getWidth(),
+      this.getHeight()
+    );
+  }
+
+  update(mousePositionX) {
+    if (mousePositionX === undefined) return;
+
+    if (mousePositionX >= 400 - this.getWidth() / 2) {
+      this.setPositionX(400 - this.getWidth());
+    } else if (mousePositionX <= this.getWidth() / 2) {
+      this.setPositionX(0);
+    } else {
+      this.setPositionX(mousePositionX - this.getWidth() / 2);
     }
-
-    #setMouseListener(canvas) {
-        canvas.addEventListener('mousemove', (event) => {
-            let rect = canvas.getBoundingClientRect();
-            let root = document.documentElement;
-
-            this.setDirection(Math.sign(event.movementX));
-            this.update(event.clientX - rect.left - root.scrollLeft);
-        })
-    }
-
-    init(posX, posY, spdX, spdY, canvas) {
-        this.#setMouseListener(canvas);
-        this.setPositionX(posX);
-        this.setPositionY(posY);
-        this.setSpeedX(spdX);
-        this.setSpeedY(spdY);
-    }
-
-    render(context) {
-      context.fillStyle = this.getColor();
-      context.fillRect(
-        this.getPositionX(),
-        this.getPositionY(),
-        this.getWidth(),
-        this.getHeight()
-      );
-    }
-
-    update(mousePositionX) {
-        if (mousePositionX === undefined) return;
-
-        if (mousePositionX >= 400 - this.getWidth()/2) {
-            this.setPositionX(400 - this.getWidth());
-        } else if (mousePositionX <=this.getWidth()/2) {
-            this.setPositionX(0);
-        } else {
-            this.setPositionX(mousePositionX - this.getWidth()/2);
-        }
-    }
-
-    getDirection() {
-        return this.#direction;
-    }
-
-    setDirection(direction) {
-        this.#direction = direction;
-    }
+  }
 }
 
-export { Player }
+export { Player };
--- src/main.js
+++ src/main.js
@@ -4,26 +4,52 @@ import { Enemy } from "./js/classes/enemy.js";
 
 let canvas, canvasContext;
 
+const CTE = {
+  //General:
+  DEBUG: false,
+  FPS: 60,
+
+  // Canvas:
+  CANVAS_WIDTH: 400,
+  CANVAS_HEIGHT: 600,
+
+  // Paddle:
+  PADDLE_WIDTH: 120,
+  PADDLE_HEIGHT: 20,
+
+  //"AI":
+  ENEMY_PRECISION: 0.4,
+
+  //Palette:
+  PRIMARY_COLOR: "#6E3EFA",
+  SECONDARY_COLOR: "#DEDEDE",
+  CONTRAST_COLOR: "#343434",
+
+  // Fonts:
+  DEBUG_FONTS: "20px Poppins, sans-serif",
+};
+
 const player = new Player(120, 20, "#6E3EFA");
 const ball = new Ball(8, "#DEDEDE");
 const enemy = new Enemy(120, 20, "#6E3EFA", 0.4);
 
 function move() {
   if (ball.colideWith(player)) {
-    let distFromCenter =
-      Math.ceil(
-        (ball.getPositionX() -
-          (player.getPositionX() + player.getWidth() / 2)) /
-          20
-      ) * 0.8;
+    let distFromCenter = player.getDistanceFromCenter(ball.getPositionX());
+    let paddleSliceSize = player.getWidth() / 6;
+    let escapeSpeed =
+      distFromCenter >= 0
+        ? Math.ceil(distFromCenter / paddleSliceSize)
+        : Math.floor(distFromCenter / paddleSliceSize);
 
     ball.setSpeedY(ball.getSpeedY() * -1);
-    ball.setSpeedX(distFromCenter * 10);
+    ball.setSpeedX((7 + Math.abs(escapeSpeed)) * Math.sign(escapeSpeed));
   }
 
   if (ball.colideWith(enemy)) {
     ball.setSpeedY(ball.getSpeedY() * -1);
   }
+
   ball.update();
   enemy.update(ball.getPositionX());
 }
@@ -37,17 +63,25 @@ function draw() {
   enemy.render(canvasContext);
 }
 
+function debug() {
+  canvasContext.fillStyle = "#DEDEDE";
+  canvasContext.font = "20px Poppins, sans-serif";
+  canvasContext.fillText(`BallSpdX: ${ball.getSpeedX()}`, 10, 20);
+  canvasContext.fillText(`BallSpdY: ${ball.getSpeedY()}`, 10, 40);
+}
+
 window.onload = function () {
   canvas = document.getElementById("gameCanvas");
   canvasContext = canvas.getContext("2d");
 
   player.init(canvas.width / 2 - 75, canvas.height - 50, 20, 0, canvas);
   enemy.init(canvas.width / 2 - enemy.getWidth() / 2, 50, 20, 0);
-  ball.init(canvas.width / 2, canvas.height / 2, 10, 10);
+  ball.init(canvas.width / 2, canvas.height / 2, 7, 7);
 
   let FPS = 60;
   setInterval(() => {
     move();
     draw();
+    CTE.DEBUG && debug();
   }, 1000 / FPS);
 };
